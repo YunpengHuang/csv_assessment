572210e545f6f22e468e0e96de7a0cf34a0e02ed
Steven <hsteven95@hotmail.co.uk>
2021-11-20 15:59:50 +1000

    add test files and unit test

--- csv_read.py
+++ csv_read.py
@@ -1,4 +1,39 @@
-import pandas as pd
+import csv
+
+import yaml
+import operator
+
+# DictReader will always convert to string
+input_csv = csv.DictReader(open("csv_text.csv"))
+
+# sort based on division and points (top 3 in the entries)
+result = sorted(input_csv, key=lambda x: (x["division"], x["points"]))[:3]
 
 
-pd.read_csv()
+
+# format that it needs to be in for it output in yaml
+data = {
+    'records': [
+        {'detail': "In division 1 from 2018-01-02 performing Offensive Duties", 'name': "zelma Ivatt"},
+        {'detail': "In division 1 from 2018-01-02 performing Offensive Duties", 'name': "zelma Ivatt"},
+        {'detail': "In division 1 from 2018-01-02 performing Offensive Duties", 'name': "zelma Ivatt"},
+    ],
+}
+
+with open("test.yaml", "w") as output_file:
+    yaml.dump(data, output_file, default_flow_style=False)
+
+    # with open("csv_result.yaml", "w") as output_file:
+    #     for rows in range(3):
+    #         yaml.dump(result[rows], output_file, default_flow_style=True)
+
+    # firstname: string
+    # lastname: string
+    # date: String
+    # division: Integer
+    # points: Integer
+    # summary: String
+
+    # sort by division then by points
+    # use dictionary python format
+    # what needs to be validated only the sorting order
---
+++ test_record.py
@@ -0,0 +1,9 @@
+import unittest
+import csv_read
+
+class TestRecord(unittest.TestCase):
+
+    def test_sort(self):
+
+
+
