34b43d074a0219054e1fe64b69fe85b7ccea7321
Steven <hsteven95@hotmail.co.uk>
2021-11-20 19:22:20 +1000

    added logging and error handling

--- csv_read.py
+++ csv_read.py
@@ -1,27 +1,55 @@
+import logging
 import csv
-
+import os
 import yaml
-import operator
 
-# DictReader will always convert to string
-input_csv = csv.DictReader(open("csv_text.csv"))
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
 
-# sort based on division and points (top 3 in the entries)
-data = sorted(input_csv, key=lambda x: (x["division"], x["points"]))[:3]
+formatter = logging.Formatter('%(asctime)s:%(levelname)s:%(message)s')
+
+file_handler = logging.FileHandler('csv_to_yaml.log')
+file_handler.setFormatter(formatter)
+
+logger.addHandler(file_handler)
+
+input_csv = {}
+
+# input from commandline
+input_csv_filename = input("input filename or the directory to the filename : ")
+output_yaml_filename = input("output filename or the directory of the output : ")
+try:
+    if os.path.getsize(input_csv_filename) > 0 and input_csv_filename.lower().endswith(
+            ".csv") and output_yaml_filename.lower().endswith(".yaml"):
+        # DictReader will always convert to string
+        input_csv = csv.DictReader(open(input_csv_filename))
+
+        # sort based on division and points (top 3 in the entries)
+        data = sorted(input_csv, key=lambda x: (x["division"], x["points"]))[:3]
 
 
-def convert_to_yaml_format(dictionary):
-    temp_dict_list = []
-    data_test = {}
-    for items in dictionary:
-        temp_dict = {"detail": "In division " + items["division"] + " from " + items["date"] + " performing " + items[
-            "summary"], "name": items["firstname"] + " " + items["lastname"]}
-        temp_dict_list.append(temp_dict)
+        def convert_to_yaml_format(dictionary):
+            """put data into a YAML recognisable format"""
+            temp_dict_list = []
+            data_test = {}
+            for items in dictionary:
+                temp_dict = {
+                    "detail": "In division " + items["division"] + " from " + items["date"] + " performing " + items[
+                        "summary"], "name": items["firstname"] + " " + items["lastname"]}
+                temp_dict_list.append(temp_dict)
 
-    data_test['record'] = temp_dict_list
-    return data_test
+            data_test['record'] = temp_dict_list
+            return data_test
 
+        # output as YAML format
+        top_three_records = convert_to_yaml_format(data)
+        with open(output_yaml_filename, "w") as output_file:
+            yaml.dump(top_three_records, output_file, default_flow_style=False)
 
-top_three_records = convert_to_yaml_format(data)
-with open("test.yaml", "w") as output_file:
-    yaml.dump(top_three_records, output_file, default_flow_style=False)
+        logger.debug('input: {}, output: {}'.format(input_csv_filename, output_yaml_filename))
+    else:
+        logger.warning('input: {}, output: {}'.format(input_csv_filename, output_yaml_filename))
+        print("file is empty or filename is not named correctly")
+except OSError as e:
+    logger.warning('input: {}, output: {}'.format(input_csv_filename, output_yaml_filename))
+    print("file does not exist")
